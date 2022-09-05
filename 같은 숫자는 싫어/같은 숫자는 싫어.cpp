690006484dfed8f0dda2a59be45bb220df4ad1b7
HYEMI YUN <yhm54321@naver.com>
2022-09-05 21:57:32 +0900

    programmers level1

---
+++ 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,26 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    vector<int> answer;
+    int i = 0;
+
+    vector<int>::iterator it = arr.begin();
+
+    answer.push_back(*it);
+    i++;
+    for (it = arr.begin() + 1; it != arr.end(); it++) {
+        if (*it != arr[i - 1]) {
+            answer.push_back(*it);
+        }
+        i++;
+    }
+
+    return answer;
+}
+
+int main() {
+    return 0;
+}
