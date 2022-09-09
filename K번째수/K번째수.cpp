bd5ed248cc57027478fac6ab78ceb529aa89cf08
HYEMI YUN <yhm54321@naver.com>
2022-09-10 00:51:05 +0900

    K번째수

---
+++ K번째수/K번째수.cpp
@@ -0,0 +1,27 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+
+    for (auto i = 0; i < commands.size(); i++) {
+        vector<int> temp;
+        for (auto j = commands[i][0] - 1; j < commands[i][1]; j++)
+            temp.push_back(array[j]);
+        sort(temp.begin(), temp.end());
+        answer.push_back(temp[commands[i][2] - 1]);
+    }
+
+    return answer;
+}
+
+int main() {
+
+    vector<int> array = { 1,5,2,6,3,7,4 };
+    vector<vector<int>> commands = { {2,5,3}, {4,4,1},{1,7,3} };
+
+    solution(array, commands);
+
+    return 0;
+}
