753a9482b59d0d3fb454bd1a2fd25ca287b6b140
HYEMI YUN <yhm54321@naver.com>
2022-09-12 12:51:06 +0900

    자릿수 더하기

---
+++ 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    string s = to_string(n);
+
+    for (auto i = 0; i < s.size(); i++)
+        answer += (s[i] - '0');
+
+    return answer;
+}
