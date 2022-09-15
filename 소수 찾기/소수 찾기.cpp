9f80f3c6622efb740351cc51b600a46c5f315e32
HYEMI YUN <yhm54321@naver.com>
2022-09-15 19:52:08 +0900

    programmers

---
+++ 소수 찾기/소수 찾기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string numbers) {
+    int answer = 0;
+    vector<char> num;
+    vector<int> prime;
+
+    for (auto i = 0; i < numbers.size(); i++)
+        num.push_back(numbers[i]);
+
+
+
+    return answer = prime.size();
+}
---
+++ 최소직사각형/최소직사각형.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int answer = 0;
+
+    for(auto i = 0; i < sizes.size(); i++)
+
+    return answer;
+}
+
+int main() {
+    vector<vector<int>> sizes = { {60, 50}, {30, 70}, {60, 30}, {80, 40} };
+
+    solution(sizes);
+}
