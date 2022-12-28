73759f4cad28de9c19e44217793f7b37a60784ae
yunhyemi <yhm54321@naver.com>
2022-12-28 22:06:50 +0900

    Programmers

--- 구명보트/구명보트.cpp
+++ 구명보트/구명보트.cpp
@@ -1,3 +1,29 @@
-//
-// Created by 윤혜미 on 2022/12/28.
-//
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    vector<int> boat;
+
+    sort(people.begin(), people.end());
+    auto p1 = people.begin();
+    auto p2 = people.end() - 1;
+
+    while(p1 <= p2){
+        if(*p1 + *p2 > limit){
+            boat.push_back(*p2);
+            p2--;
+        }
+        else if(*p1 + *p2 <= limit){
+            boat.push_back(*p1 + *p2);
+            p1++;
+            p2--;
+        }
+        else if(p1 == p2)
+            boat.push_back(*p1);
+    }
+    return answer = boat.size();
+}
--- 행렬의 덧셈/행렬의 덧셈.cpp
+++ 행렬의 덧셈/행렬의 덧셈.cpp
@@ -1,3 +1,18 @@
-//
-// Created by 윤혜미 on 2022/12/28.
-//
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer; //2차원 벡터 정적할당 및 초기화
+    vector<int> arr;
+
+    for(auto i = 0; i < arr1.size(); i++){
+        for(auto j = 0; j < arr1[0].size(); j++)
+            arr.push_back(arr1[i][j]+arr2[i][j]);
+        answer.push_back(arr);
+        arr.clear();
+    }
+
+    return answer;
+}
