783e31b6a6dd2cc014c889a5c6c059a5a27875cb
yunhyemi <yhm54321@naver.com>
2022-12-29 01:18:55 +0900

    Programmers

--- 체육복/체육복.cpp
+++ 체육복/체육복.cpp
@@ -1,25 +1,28 @@
 #include <vector>
-#include <algorithm>
-#include <iostream>
+#include <map>
 
 using namespace std;
-
 int solution(int n, vector<int> lost, vector<int> reserve) {
-    int answer = n-lost.size();
-    auto i = lost.begin();
-    auto j = reserve.begin();
+    int answer = 0;
+    map<int,int> student;
 
-    for(auto i = lost.begin(); i != lost.end(); i++)
-        if(!reserve.empty())
-            for(auto j = reserve.begin(); j != reserve.end(); j++){
-                if(*i == *j + 1 || *i == *j - 1){
-                    cout << *j << endl;
-                    reserve.erase(j);
-                    lost.erase(i);
-                    answer +=1;
-                    cout << *j << endl;
-                }
+    for(auto i:lost) student[i] -= 1; // lost벡터의 값을 인덱스로 이용
+    for(auto i:reserve) student[i] += 1; // reserve벡터의 값을 인덱스로 이용
+
+    for(auto i = 1; i <= n; i++){
+        if(student[i] == -1){
+            if(student[i-1] == 1){
+                student[i] = student[i-1] = 0;
             }
+            else if(student[i+1] == 1){
+                student[i] = student[i+1] = 0;
+            }
+        }
+    }
+
+    for(auto i = 1; i <= n; i++)
+        if(student[i] != -1)
+            answer++;
 
     return answer;
 }
