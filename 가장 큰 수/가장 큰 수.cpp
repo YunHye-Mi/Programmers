38e6d7305513e26862242fb6fe8c45cb8fe32385
yunhyemi <yhm54321@naver.com>
2022-12-30 00:42:32 +0900

    Programmers

---
+++ 가장 큰 수/가장 큰 수.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int compare(string a, string b){
+    return a + b > b + a;
+}
+
+string solution(vector<int> numbers) {
+    string answer = "";
+    vector<string> num;
+
+    for(auto i:numbers){
+        num.push_back(to_string(i));
+    }
+
+    sort(num.begin(),num.end(), compare);
+
+    for(auto s:num)
+        answer += s;
+
+    if(answer[0] == '0') return "0";
+
+    return answer;
+}
